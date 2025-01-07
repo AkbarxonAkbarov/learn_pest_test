05025117bb2ef446eb72f4916001f47250af9726
Akbarxon <akbarxon9400@gmail.com>
2025-01-07 16:00:55 +0500

    save_select

--- tests/Feature/UserControllerTest.php
+++ tests/Feature/UserControllerTest.php
@@ -1,18 +1,16 @@
 <?php
 
 use Tests\TestCase;
-// use Illuminate\Foundation\Testing\RefreshDatabase;
+//  use Illuminate\Foundation\Testing\RefreshDatabase;
 use App\Models\User;
 use App\Models\Author;
 
-// Autohorisation tekshirish
 it("has authorization check", function () {
     $response = $this->withHeaders(["Accept" => "application/json"])->get("/api/authors");
 
     $response->assertStatus(401);
 });
 
-// HTTP Status kodini tekshirish
 
 it("returns success http status code", function () {
     $user = new User();
@@ -31,7 +29,6 @@ it("returns success http status code", function () {
 
     $response->assertStatus(200);
 });
-    // Mijozlarni tekshirish
 
 it("shows list of clients without pagination", function () {
     $user = new User();
@@ -85,10 +82,11 @@ it("checks error response in error case", function () {
     $user->email = 'test_email';
     $user->password = bcrypt('password');
     $user->save();
+    $user->delete();
+    $user->select("*")->get();
 
     $token = $user->createToken("test_token");
 
-    // Error response test
     $responseToAssert = [
         "data" => [
             "message" => "error message",
